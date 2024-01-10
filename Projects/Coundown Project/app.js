6fed86b18c05a5bfbf16e428859a12417faee612
Muhammad Ihtesham Khan <khanihtesham0332@gmail.com>
2024-01-10 18:31:10 +0500

    count down small project

---
+++ Projects/Coundown Project/app.js
@@ -0,0 +1,24 @@
+const End_date = "Date: 13 June 2024 04:00:AM";
+document.getElementById("Endtime").innerHTML = End_date
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+    const end = new Date(End_date)
+    const now = new Date();
+    const diff = (end - now) / 1000;
+    if (diff < 0) {
+        return
+    }
+    //convetring to hours
+
+    inputs[0].value = Math.floor(diff / 3600 / 24)
+    inputs[1].value = Math.floor(diff / 3600) % 24
+    inputs[2].value = Math.floor(diff / 60) % 60
+    inputs[3].value = Math.floor(diff) % 60
+}
+
+clock();
+
+setInterval(() => {
+    clock()
+}, 1000)
